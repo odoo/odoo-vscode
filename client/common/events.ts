9c8125c0a17a3c1b47ec0d952076670fd3561222
fda-odoo <fda@odoo.com>
2025-08-18 17:52:21 +0200

    [NEW] Move VsCode extension to odoo-ls-vscode

---
+++ client/common/events.ts
@@ -0,0 +1,3 @@
+import {EventEmitter} from "vscode";
+
+export const clientStopped = new EventEmitter();
---
+++ client/common/validation.ts
@@ -0,0 +1,16 @@
+export const STATE_VERSION = 100
+
+export const stateInit = {
+    "Odoo.nextConfigId": 0,
+    "Odoo.stateVersion": STATE_VERSION,
+}
+
+export function getConfigurationStructure(id: number = 0) {
+    return {
+        "id": id,
+        "name": `New Configuration ${id}`,
+        "odooPath": "",
+        "addons": [],
+        "validatedAddonsPaths": [],
+    }
+}
